bbde5db008b38e48ebde3b72e7242c93f9ed0c3e
TOXIC_BOT1971 <118433674+toxic329@users.noreply.github.com>
2023-02-03 21:01:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+API_ID = int(os.getenv("28766857"))
+API_HASH = os.getenv("f75afe896b3120bd13ca0adc24e6a72df75afe896b3120bd13ca0adc24e6a72d")
+BOT_TOKEN = os.getenv("6155008157:AAHmbJp1_qPH2zePRcCCzXfsL6m_ZCSdnvQ6155008157:AAHmbJp1_qPH2zePRcCCzXfsL6m_ZCSdnvQ")
+DATABASE_URL = os.getenv("mongodb+srv://Prince474:<password>@cluster0.z5dz5bf.mongodb.net/?retryWrites=true&w=majoritymongodb+srv://Prince474:<password>@cluster0.z5dz5bf.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = list({int(x) for x in os.environ.get(""5911274691"").split()})
